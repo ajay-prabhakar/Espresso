71e00299b205f4ef844638c5d3d0b2998e20fdc8
Chromicle <ajayprabhakar369@gmail.com>
2019-08-11 16:52:50 +0530

    Added first test

--- app/src/androidTest/java/com/example/android/teatime/orderActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/teatime/orderActivityBasicTest.java
@@ -1,7 +1,44 @@
 package com.example.android.teatime;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
 /**
  * Created by Chromicle on 11/8/19.
  */
+
+@RunWith(AndroidJUnit4.class)   //launching app and control UI tests with special processing
 public class orderActivityBasicTest {
+    @Rule
+    public ActivityTestRule<OrderActivity> mActiviyTestRule
+            = new ActivityTestRule<>(OrderActivity.class); //functional testing for single activity
+
+    @Test  // starting test
+    public void clickDecrementButton() {
+        // Check the initial quantity variable is zero
+        onView((withId(R.id.quantity_text_view))).check(matches(withText("0")));
+
+        // Click on decrement button
+        onView((withId(R.id.decrement_button)))
+                .perform(click());
+
+        // Verify that the decrement button decreases the quantity by 1
+        onView(withId(R.id.quantity_text_view)).check(matches(withText("0")));
+
+        // Verify that the increment button also increases the total cost to $5.00
+        onView(withId(R.id.cost_text_view)).check(matches(withText("$0.00")));
+
+    }
+
 }
